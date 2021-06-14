11386a0a99256a4002dc84a80e662bcddf327ba5
Marcos <exegeses@gmail.com>
2021-06-14 20:57:04 -0300

    vistas de Paneles de administración de Regiones y de Destinos

---
+++ agencia/clases/Region.php
@@ -0,0 +1,43 @@
+<?php
+
+    class Region
+    {
+        private $regID;
+        private $regNombre;
+
+
+
+        ####################################
+        ### getters & setters
+        /**
+         * @return mixed
+         */
+        public function getRegID()
+        {
+            return $this->regID;
+        }
+
+        /**
+         * @param mixed $regID
+         */
+        public function setRegID($regID)
+        {
+            $this->regID = $regID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getRegNombre()
+        {
+            return $this->regNombre;
+        }
+
+        /**
+         * @param mixed $regNombre
+         */
+        public function setRegNombre($regNombre)
+        {
+            $this->regNombre = $regNombre;
+        }
+    }
